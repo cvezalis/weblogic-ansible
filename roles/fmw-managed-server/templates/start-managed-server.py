8bc2f269cdb2fcd393f00a06eea23532fd158f9c
cvezalis <christos.vezalis@gmail.com>
2015-09-28 10:56:15 +0300

    Revert the way managed server starts

--- roles/fmw-managed-server/templates/start-managed-server.py
+++ roles/fmw-managed-server/templates/start-managed-server.py
@@ -1,8 +1,6 @@
-#connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
-#start('{{ managed_server_name }}')
+connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
+start('{{ managed_server_name }}')
 
-nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_listen_address }}', '{{ node_manager_listen_port }}', '{{ domain_name }}');
-
-nmStart('{{ managed_server_name }}');
-
-nmDisconnect();
+#nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_listen_address }}', '{{ node_manager_listen_port }}', '{{ domain_name }}');
+#nmStart('{{ managed_server_name }}');
+#nmDisconnect();
