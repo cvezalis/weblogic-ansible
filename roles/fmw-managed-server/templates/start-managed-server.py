ddea90dda46acce3b8908b40af0c2e68d4b0b3da
cvezalis <christos.vezalis@gmail.com>
2015-09-28 10:29:37 +0300

    Change the way managed server starts

--- roles/fmw-managed-server/templates/start-managed-server.py
+++ roles/fmw-managed-server/templates/start-managed-server.py
@@ -1,8 +1,8 @@
-#nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_listen_address }}', '{{ node_manager_listen_port }}', '{{ domain_name }}');
+#connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
+#start('{{ managed_server_name }}')
 
-#nmStart('{{ managed_server_name }}');
+nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_listen_address }}', '{{ node_manager_listen_port }}', '{{ domain_name }}');
 
-#nmDisconnect();
+nmStart('{{ managed_server_name }}');
 
-connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
-start('adfsrv1')
+nmDisconnect();
